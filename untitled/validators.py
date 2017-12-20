417fb1448f9eaa8ca26ba1896983d6baa7ee97e4
InnaKardahina <innamamaeva@mail.ru>
2017-12-20 23:38:31 +0500

    Correction of mistakes

--- untitled/validators.py
+++ untitled/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 class name_and_slug_validator(object):
 
-    def __call_():
+    def __call_(self, value):
         message = "This field cannot be longer than 200 symbols."
         code = 'Invalid'
         if len(value) > 200:
@@ -12,7 +12,7 @@ class name_and_slug_validator(object):
 
 class interf_capacity_validator(object):
 
-    def __call_():
+    def __call_(self, value):
         message = "This field cannot be longer than 30 symbols."
         code = 'Invalid'
         if len(value) > 30:
