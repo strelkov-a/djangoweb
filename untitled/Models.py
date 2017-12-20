446be949f3b18822ea0615e2cd64c4a71b337ae4
strelkov-a <alexander.strelkov@hotmail.com>
2017-12-20 22:18:43 +0500

    Added constructors to classes

--- untitled/Models.py
+++ untitled/Models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
@@ -41,6 +44,9 @@ class Product(models.Model):
         ]
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def __str__(self):
         return self.name
